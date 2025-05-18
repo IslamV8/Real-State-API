c30881a361d79dd630d172de5e691ca9cccb7035
IslamV8 <eslamsavage@gmail.com>
2025-05-18 09:32:54 +0300

    Fix: add missing serverless require

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const serverless = require("serverless-http");
 
 app.use(express.json());
 
